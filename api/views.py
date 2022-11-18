99f3b4b3067c4c071403829fcee138a2f9d0d171
Brian Mungai <bryanmungai254@gmail.com>
2022-11-18 16:19:09 +0300

    added a generic class view - RoomView

--- api/views.py
+++ api/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
 
 
-def main(request):
-    return HttpResponse("Hello")
+class RoomView(generics.ListAPIView):
+    # get all room objects
+    queryset = Room.objects.all()
+    # pass to serializer to convert queryset to json object
+    serializer_class = RoomSerializer
+
+# @api_view(['GET'])
+# def RoomView(request):
+#     # get all room objects
+#     queryset = Room.objects.all()
+#     #     # pass to serializer to convert queryset to json object
+#     serializer_class = RoomSerializer(queryset, many=True)
+
+#     return Response(serializer_class.data)
